bcf3435efa55ff3182d471dd633e2b6790cd979d
JongHooon <qnm541@gmail.com>
2023-01-15 15:16:24 +0900

    Initial Commit

---
+++ TodoAppTutorial/TodoAppTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppTutorialApp.swift
+//  TodoAppTutorial
+//
+//  Created by JongHoon on 2023/01/15.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
